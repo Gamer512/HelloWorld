a727ff7fc866192bfab88d605567b57fe7c45290
Stephan Beurer <Stephan Beurer>
2017-02-06 16:00:19 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/example/stephan/helloworld/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.stephan.helloworld;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Stephan on 06.02.2017.
+ */
+public class MainActivityTest {
+
+}
---
+++ app/src/main/java/com/example/stephan/helloworld/Config.java
@@ -0,0 +1,10 @@
+package com.example.stephan.helloworld;
+
+/**
+ * Created by Stephan on 06.02.2017.
+ */
+
+public class Config {
+    public static final String EMAIL ="stephan.beurer@gmail.com";
+    public static final String PASSWORD ="PM(4)ms1";
+}
